23b0d91ca1e9b9abb2433a8e09c66e42c2756507
V <shruthipv619@gmail.com>
2019-01-24 18:04:12 +0530

    Completed 2

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -14,7 +14,6 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5)
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
-
             var step = (int)Math.Ceiling(Students.Count * 0.2);
 
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
@@ -29,6 +28,7 @@ namespace GradeBook.GradeBooks
                 return 'D';
             else
                 return base.GetLetterGrade(averageGrade);
+
 
         }
     }
