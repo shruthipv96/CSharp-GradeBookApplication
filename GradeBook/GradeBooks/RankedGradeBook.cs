c2a8c5d132ddcc106f8e0f02aeb88c6318ad3000
V <shruthipv619@gmail.com>
2019-01-24 18:08:06 +0530

    Completed 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,23 +13,21 @@ namespace GradeBook.GradeBooks
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
-                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
-            var step = (int)Math.Ceiling(Students.Count * 0.2);
-
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
-            if (grades[step - 1] <= averageGrade)
+            //Return the letter grade based upon the rank
+            if (grades[threshold - 1] <= averageGrade)
                 return 'A';
-            else if (grades[step * 2 - 1] <= averageGrade)
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
                 return 'B';
-            else if (grades[step * 3 - 1] <= averageGrade)
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
                 return 'C';
-            else if (grades[step * 4 - 1] <= averageGrade)
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
                 return 'D';
             else
-                return base.GetLetterGrade(averageGrade);
-
-
+                return 'F';
         }
     }
 }
